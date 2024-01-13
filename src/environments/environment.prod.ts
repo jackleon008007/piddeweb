4d60afdd0b01a8666902d1692f668d797a6558a8
jack <jack008007z@outlook.es>
2024-01-12 20:28:52 -0500

    update all

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBLh-zYb4aIFQTut2SnJHpnsA3XiHStMMM",
+    authDomain: "pidde-69292.firebaseapp.com",
+    projectId: "pidde-69292",
+    storageBucket: "pidde-69292.appspot.com",
+    messagingSenderId: "241072674587",
+    appId: "1:241072674587:web:3bc871ea0c5d276fcb65be",
+    measurementId: "G-NEZMBSTHKS"
+  }
 };
+
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import {enableProdMode} from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import { AppModule } from './app/app.module';
 import {environment} from "./environments/environment";
 
 if(environment.production){
   enableProdMode();
 }
+
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
